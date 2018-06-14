4f1413db7c22ee1a6b17dbba9ef5950478c9bbe0
cody <codyjeromegreen@gmail.com>
2018-06-13 18:53:51 -0700

    working on the api still

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.get('/callback', (req, res) => {
         res.cookie('X-Some Cookie', idTokenPayload);
         res.write('<h1>' + json.name + '</h1>');
         res.write('<h1>' + json.email + '</h1>');
-        res.write('<h1>' + json.picture + '</h1>');
+        res.write('<img src=' + json.picture + '>');
         res.end();
       })
 
